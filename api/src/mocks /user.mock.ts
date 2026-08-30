da58afca50c459c94ea5e6190d9e00ecdc1884d8
willianyahiro <teresinharomualdo@gmail.com>
2026-08-30 14:43:29 +0000

    feat: adicionar dados simulados

---
+++ api/src/mocks /user.mock.ts
@@ -0,0 +1,21 @@
+import type { user } from '../types/user.types.ts';
+
+
+const users: user[] = [{
+    id : 1,
+    name : 'yahiro',
+    email : 'yahiro@mainModule.com',
+    password : 'yahiro'
+}, {
+    id: 2,
+    name : 'willian',
+    email : 'willian@gmail.com',
+    password : 'willian'
+}, {
+    id : 3,
+    name : 'yamamoto',
+    email : 'yamamoto@gmail',
+    password : 'yamamoto'
+}];
+
+export default users;
